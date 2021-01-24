d3e2b027b21e9a87662bf7c5c96d989be16da15e
Francuski Miroslav <francuski.miroslav@gmail.com>
2021-01-25 00:30:09 +0100

    Updates

---
+++ precache-manifest.e3867ab5607cb1886a6314e2bc24c904.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a545a0acf70a98830ddcc6f9c68cbf98",
+    "url": "/REACTappCocktails/index.html"
+  },
+  {
+    "revision": "31ec98d2a4c195ceda4a",
+    "url": "/REACTappCocktails/static/css/main.7e57030a.chunk.css"
+  },
+  {
+    "revision": "64f988e04ec47ce486dc",
+    "url": "/REACTappCocktails/static/js/2.ff876b9d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/REACTappCocktails/static/js/2.ff876b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ec98d2a4c195ceda4a",
+    "url": "/REACTappCocktails/static/js/main.6bbfcd8f.chunk.js"
+  },
+  {
+    "revision": "c0ec97be3cd4179b9f0f",
+    "url": "/REACTappCocktails/static/js/runtime-main.ab9afdaf.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/REACTappCocktails/static/media/logo.9a3d2645.svg"
+  }
+]);
