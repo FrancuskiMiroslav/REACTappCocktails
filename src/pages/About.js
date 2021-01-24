8c4aca879d3e3cd5082c759fd927a56a2a62142b
Francuski Miroslav <francuski.miroslav@gmail.com>
2021-01-25 00:19:56 +0100

    added routes and easy pages error and about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+	return (
+		<section className='section about-section'>
+			<h1 className='section-title'>about page</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta mollitia
+				consequatur fuga hic quam officia deleniti, sequi nemo, laborum
+				obcaecati explicabo repellendus, consequuntur eaque. Obcaecati magnam
+				officia nemo aut nostrum porro dolore a nihil officiis? Sint cumque
+				ipsam laudantium officia minus, molestiae alias enim saepe quam eum,
+				dolore ratione culpa.
+			</p>
+		</section>
+	);
+};
 
-export default About
+export default About;
