a1db81614f87e9bd30a3a27d51c2083a96fc00b4
Francuski Miroslav <francuski.miroslav@gmail.com>
2021-01-26 00:47:24 +0100

    Updates

--- precache-manifest.d062fe04fd1123f33366fff628d10d87.js
+++ precache-manifest.d062fe04fd1123f33366fff628d10d87.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d15699579bb2a57929531ed8aa52787a",
+    "revision": "0597b66c2a349b8ea880092da5b0f7e8",
     "url": "/REACTappCocktails/index.html"
   },
   {
-    "revision": "0f575c08e472ed0a93de",
+    "revision": "0a8abbe2ccb57923ad7e",
     "url": "/REACTappCocktails/static/css/main.7e57030a.chunk.css"
   },
   {
-    "revision": "f2506d1dd103360ca326",
-    "url": "/REACTappCocktails/static/js/2.9d804b93.chunk.js"
+    "revision": "e8002103d6ae27529ef7",
+    "url": "/REACTappCocktails/static/js/2.5e436efa.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/REACTappCocktails/static/js/2.9d804b93.chunk.js.LICENSE.txt"
+    "url": "/REACTappCocktails/static/js/2.5e436efa.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0f575c08e472ed0a93de",
-    "url": "/REACTappCocktails/static/js/main.3e08924d.chunk.js"
+    "revision": "0a8abbe2ccb57923ad7e",
+    "url": "/REACTappCocktails/static/js/main.df4645f7.chunk.js"
   },
   {
     "revision": "c0ec97be3cd4179b9f0f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/REACTappCocktails/precache-manifest.c15d94f648e9b7721c8f9d96ae16cbb8.js"
+  "/REACTappCocktails/precache-manifest.d062fe04fd1123f33366fff628d10d87.js"
 );
 
 self.addEventListener('message', (event) => {
